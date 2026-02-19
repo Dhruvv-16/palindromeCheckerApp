149120d57cc6fa1dde021689174083403f842446
Dhruvv-16 <dhruvvachhani987@gmail.com>
2026-02-19 10:08:12 +0530

    UC3:String Reverse palindrome

---
+++ src/UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class UseCase3PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+        int length = original.length();
+        String reversed = "";
+        for (int i = length - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println("It is a Palindrome.");
+        } else {
+            System.out.println("It is NOT a Palindrome.");
+        }
+        scanner.close();
+    }
+}
